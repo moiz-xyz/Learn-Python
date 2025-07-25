0fb656ddd7666cda93320bf6e6d465080944e828
moiz-xyz <a.moizniaxi123@gmail.com>
2025-07-25 07:23:36 -0700

    added variables and data types

---
+++ 02_variables and data-types/main.py
@@ -0,0 +1,46 @@
+# sting variable 
+username = "John elia"
+print ("Hello my name is" , username)
+
+# integer varible
+age  = 12
+print ("this is my age " , age) 
+
+length = 10
+width = 20
+
+print(f"The area  of a box with length of {length} and with of {width} will be  " , length * width)
+
+
+# float variables 
+float = 10.234
+print (float)
+
+
+#boolean variables 
+isAvailable = True
+
+print("Is available" , isAvailable)  # true 
+
+# checking if it adds string and no
+num1 = 12 
+num2 = "20"
+
+print (num1 + num2) # error 
+
+num1 = 12 
+num2 = 20
+
+print (num1 + num2) # 32
+
+
+# Rules:
+# Should not start with a number
+# Should not contain special characters except underscore (_)
+# Should not be a reserved keyword in Python
+# Should be descriptive and meaningful
+# Should be in lowercase letters
+# Should be consistent in naming convention (e.g., snake_case or camelCase)
+# Should be unique within the same scope
+# Should be easy to read and understand
+
