cd01ee52b2831361914e1350f162392e8e2cb41b
moiz-xyz <a.moizniaxi123@gmail.com>
2025-08-03 13:04:52 +0500

    added arithematic operation in python

---
+++ 03_Arithmetic operations/main.py
@@ -0,0 +1,37 @@
+num1 = 10
+num2 = 5
+
+# Addition
+result_add = num1 + num2
+print(f"Addition >>  {num1} + {num2} = {result_add}")
+
+# Subtraction
+result_sub = num1 - num2
+print(f"Subtraction >>  {num1} - {num2} = {result_sub}")
+
+# Multiplication
+result_mul = num1 * num2
+print(f"Multiplication >>  {num1} * {num2} = {result_mul}")
+
+# it will return division result as float numbers
+# Division
+result_div = num1 / num2   
+print(f"Division >>  {num1} / {num2} = {result_div}")
+
+#  it would return whole number after rounding numbers
+# Floor Division
+result_floor_div = num1 // num2 
+print(f"Floor Division >>  {num1} // {num2} = {result_floor_div}") 
+
+#  rmeinder
+result_rem = num1 % num2
+print(f"Remainder >>  {num1} % {num2} = {result_rem}")
+
+# Exponentiation
+result_exp = num1 ** num2  
+print(f"Exponentiation >>  {num1} ** {num2} = {result_exp}")
+
+
+# absolute value
+num3 = -15
+print(f"Absolute Value >>  |{num3}| = {abs(num3)}")
