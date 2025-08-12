24573310aca35231153e83478b539eb5106e1327
moiz-xyz <a.moizniaxi123@gmail.com>
2025-08-12 16:06:01 +0500

    added  taking of input from user

---
+++ 09_userInput/main.py
@@ -0,0 +1,17 @@
+print ("Hello")
+
+name = "Ali"
+print ("name")
+
+name = input("What is your name")
+print(f'Your name is {name}')
+
+user = input("What is your name")
+age = input("What is your age")
+print(f'Hello {name} , you are {age} years old')
+
+num1 = int ( input("Enter first Number") )
+num2 = int ( input("Enter second Number") )
+sum = num1 + num2
+
+print(f"The sum of {num1} , and {num2} ,is" ,{sum})
