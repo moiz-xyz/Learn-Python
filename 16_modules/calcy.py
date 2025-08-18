4cf33b2d998f041b5358342ec776113545367e28
moiz-xyz <a.moizniaxi123@gmail.com>
2025-08-18 16:50:56 +0500

    added how to use modeules in python

---
+++ 16_modules/calcy.py
@@ -0,0 +1,14 @@
+def add(a, b):
+    return a + b
+
+def sub(a, b):
+    return a - b
+
+def multiply(a, b):
+    return a * b
+
+def even_or_odd(num):
+    if num % 2 == 0:
+        return "Even"
+    else:
+        return "Odd"
---
+++ 16_modules/main.py
@@ -0,0 +1,7 @@
+import calcy
+
+
+print(calcy.add(5, 6))       # 11
+print(calcy.sub(16, 5))      # 11
+print(calcy.multiply(5, 6))  # 30
+print(calcy.even_or_odd(5))  # Odd
